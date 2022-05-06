08d8b579d40a3c33c73237e2e86d4b6bc95c30db
Abbos Mamadaliyev <abbbos166@gmail.com>
2022-05-06 08:06:37 +0500

    architecture

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  final Color orange = const Color(0xffff702f);
+  final Color orangeLight = const Color(0xffffa032);
+  final Color green = const Color(0xff149305);
+}
